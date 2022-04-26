f6d8ab39a17ade9f715e9a0ded1da967725596c4
Seema Taneja <seemamehta001@gmail.com>
2022-04-26 11:41:44 +0100

    Print FizzBuzz when the number is multiple of 3 and 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,13 @@
 
 for(let i = 1 ; i <=100; i++){
-   if( i % 3 == 0){
+   if( i % 3 == 0 && i % 5 == 0){
+       console.log("FizzBuzz");
+   }else if( i % 3 == 0){
        console.log("Fizz");
-   }else{
+   }else if( i % 5 == 0){
+       console.log("Buzz");
+   }
+   else{
        console.log(i);
    }
 }
