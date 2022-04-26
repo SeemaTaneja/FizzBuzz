e6810a132977792749a490563798ac8405e26fb4
Seema Taneja <seemamehta001@gmail.com>
2022-04-26 19:04:32 +0100

    Multiple of 3,5,7 11 and 13

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,25 @@
 
 for(let i = 1 ; i <=100; i++){
     let fizzBuzzString = "";
-    if( i % 3 == 0){
+    if( i % 3 === 0){
       fizzBuzzString += "Fizz";
    }
-   if( i % 5 == 0){
+   if( i % 5 === 0){
        fizzBuzzString += "Buzz";
+   }
+   if( i % 7 === 0){
+       fizzBuzzString += "Bang";
+   }
+   if( i % 11 === 0){
+       fizzBuzzString = "Bong";
+   }
+   if( i % 13 === 0){
+       const b = fizzBuzzString.indexOf("B");
+       if(b == -1){
+           fizzBuzzString += "Fezz";
+       }else{
+           fizzBuzzString = fizzBuzzString.slice(0 , b) + "Fezz" + fizzBuzzString.slice(b);
+       }
    }
    if(fizzBuzzString === ""){
     fizzBuzzString += i;
